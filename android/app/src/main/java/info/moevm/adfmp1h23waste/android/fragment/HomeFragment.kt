420a96e63a7a5d2ea0b0d7530d33b9fddaf2bec8
maikleProko <maiklProko@gmail.com>
2023-03-22 22:26:17 +0300

    Update home page (Edit kt)

--- android/app/src/main/java/info/moevm/adfmp1h23waste/android/fragment/HomeFragment.kt
+++ android/app/src/main/java/info/moevm/adfmp1h23waste/android/fragment/HomeFragment.kt
@@ -4,13 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import info.moevm.adfmp1h23waste.android.R
+import info.moevm.adfmp1h23waste.android.databinding.AchievementBinding
 import info.moevm.adfmp1h23waste.android.databinding.FragmentHomeBinding
-import info.moevm.adfmp1h23waste.android.user.UserManager
+import info.moevm.adfmp1h23waste.android.user.Achievement
+import kotlin.math.round
+
 
 class HomeFragment : Fragment() {
 
@@ -25,7 +26,7 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
 
         setUpTransitionButtons()
-        fillAchievements(binding.achievementsLayout)
+        fillAchievements()
 
         return binding.root
     }
@@ -39,16 +40,32 @@ class HomeFragment : Fragment() {
         }
     }
 
-    private fun fillAchievements(layout: LinearLayout) {
-        if (UserManager.instance.user == null) {
-            /*layout.addView(TextView(this.context).apply {
-                this.text = "Достижения доступны только авторизованным пользователям"
-            })*/
-        }
+    private fun fillAchievement(achievementBinding: AchievementBinding, achievement: Achievement) {
+        val linkTitle = achievementBinding.title
+        val linkDescription = achievementBinding.description
+
+        linkTitle.text = achievement.name
+        linkDescription.text = "Выполнено " +
+                round(achievement.current).toInt().toString() +
+                " из " + round(achievement.max).toInt().toString()
+    }
+
+    private fun fillAchievements() {
+        fillAchievement(binding.achievement1, Achievements.FIRST_ACHIEVEMENT.achievement)
+        fillAchievement(binding.achievement2, Achievements.SECOND_ACHIEVEMENT.achievement)
+        fillAchievement(binding.achievement3, Achievements.THIRD_ACHIEVEMENT.achievement)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+
+    enum class Achievements(val achievement: Achievement) {
+        FIRST_ACHIEVEMENT(Achievement("Всего заданий", 94.0, 365.0)),
+        SECOND_ACHIEVEMENT(Achievement( "Подряд без ошибок", 94.0, 365.0)),
+        THIRD_ACHIEVEMENT(Achievement( "Процент правильных", 94.0, 365.0))
+    }
 }
+
